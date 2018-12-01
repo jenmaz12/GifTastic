9524bbda3c8d08d6d5c473152158bc20ca357cab
jenmaz12 <jenmaz12@gmail.com>
2018-12-01 17:43:44 -0500

    render buttons

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    var topics = ["dogs","cats","mouse","moose","duck","horse","lion","giraffe","hamster","hippo","cow","chicken","pig"];
+
+    function renderButtons() {
+        // empty buttons div to prevent duplicates
+        $("#buttons").empty();
+        
+        for(i=0; i<topics.length; i++) {
+            // create new button
+            var a= $("<button>");
+            // add bootstrap button class
+            a.addClass("btn");
+            a.addClass("btn-info");
+            // add attribute of data-name equal to the item in the array
+            a.attr("data-name",topics[i]);
+            // set button text equal to the item in the array
+            a.text(topics[i]);
+            // append new button to buttons div
+            $("#buttons").append(a);
+        }
+    }
+    
+    renderButtons();
+});
+
